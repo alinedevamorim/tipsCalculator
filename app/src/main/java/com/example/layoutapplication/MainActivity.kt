ac47044c8befe74a38855d2445e90ffef4f0ff2f
alinedevamorim <a.li.neh@hotmail.com>
2024-12-06 13:01:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.layoutapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+
+
+}
