ecf47ffa3a35b6e9a7696e9a45f5a7db0b0cf820
alinedevamorim <a.li.neh@hotmail.com>
2024-12-07 01:13:21 -0300

    onCreate

--- app/src/main/java/com/example/layoutapplication/MainActivity.kt
+++ app/src/main/java/com/example/layoutapplication/MainActivity.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    // Valor total conta
+    // Numero de pessoas
+    // Porcentagem de gorjeta
+     // 10 , 15 ou 20%
+    // Calcular
+    // Limpar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
