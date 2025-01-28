e2a1cba93ab38617a2cb28ea668478a6efa23407
alinedevamorim <a.li.neh@hotmail.com>
2025-01-27 22:38:04 -0300

    onCreate

---
+++ app/src/main/java/com/example/layoutapplication/SummaryActivity.kt
@@ -0,0 +1,35 @@
+package com.example.layoutapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.layoutapplication.databinding.ActivitySummaryBinding
+
+class SummaryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySummaryBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySummaryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val totalTable = intent.getFloatExtra("totalTable", 0.0f)
+        val numPeople = intent.getIntExtra("numPeople", 0)
+        val percentage = intent.getIntExtra("percentage", 0)
+        val totalAmount = intent.getFloatExtra("totalWithTips", 0.0f)
+
+        binding.tvPercentage.text = percentage.toString()
+        binding.tvTotalAmount.text = totalAmount.toString()
+        binding.tvTotalTable.text = totalTable.toString()
+        binding.tvNumPeopleTable.text = numPeople.toString()
+
+        binding.btnRefresh.setOnClickListener {
+            finish()
+        }
+
+
+    }
+}
